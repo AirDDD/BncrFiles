880b28d00dc8cd3b674e295395a5121924deef7d
AirDDD <airccc@outlook.com>
2025-09-14 22:25:46 +0800

    Create 黑丝.js

---
+++ plugins/Air/黑丝.js
@@ -0,0 +1,28 @@
+/**
+ * @author Air
+ * @team 阳光
+ * @name 黑丝
+ * @version 1.0.0
+ * @description 发送“黑丝”后自动获取视频
+ * @rule ^黑丝$
+ * @priority 10
+ * @admin false
+ * @public true
+ * @encrypt false
+ * @disable false
+ * @classification ["视频"]
+ */
+
+
+const got = require('got');
+
+module.exports = async s => {
+    const apiUrl = 'https://api.yujn.cn/api/heisis.php?type=json';
+    const response = await got(apiUrl);
+    const data = JSON.parse(response.body);
+    const videoUrl = data.data.trim();
+    await s.reply({
+      type: 'video',
+      path: videoUrl,
+    });
+};
